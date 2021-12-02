f00e23ee95e9948350795c363c613c1c53140b10
asketsystem <asketsystem1@gmail.com>
2021-12-02 17:07:34 +0000

    Nav & Footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 
+import Footer from './Footer';
+import Navbar from './Navbar';
+
 export default function Layout({ children }) {
   return (
     <>
@@ -9,11 +12,11 @@ export default function Layout({ children }) {
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
-          
+          <Navbar />
         </header>
         <main>{children}</main>
         <footer>
-         
+          <Footer />
         </footer>
       </Box>
     </>
